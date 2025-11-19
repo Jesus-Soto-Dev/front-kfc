f62974ef521d37aaece653aa8f5c16acc35048fb
ritaluc <ritalusan04@gmail.com>
2025-11-19 20:37:13 +0100

    corregidos errores de responsive y formulario de datos personales creado

---
+++ componentes/formularioDatosPersonales/js/formularioDatosPersonales.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    const form = document.getElementById("form-personales");
+    const inputs = form.querySelectorAll("input, select");
+
+    // --- RELLENAR DÍAS ---
+    const diaSelect = document.getElementById("dia");
+    for (let i = 1; i <= 31; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        diaSelect.appendChild(option);
+    }
+
+    // --- RELLENAR AÑOS ---
+    const anoSelect = document.getElementById("ano");
+    const currentYear = new Date().getFullYear();
+
+    for (let i = 0; i < 100; i++) {
+        const year = currentYear - i;
+        const option = document.createElement("option");
+        option.value = year;
+        option.textContent = year;
+        anoSelect.appendChild(option);
+    }
+
+
+    function validar(input) {
+        input.classList.remove("is-invalid");
+
+        const value = input.value.trim();
+
+        // Requeridos
+        if (input.hasAttribute("required") && !value) {
+            input.classList.add("is-invalid");
+            return false;
+        }
+
+        return true;
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("blur", () => validar(input));
+    });
+})();
